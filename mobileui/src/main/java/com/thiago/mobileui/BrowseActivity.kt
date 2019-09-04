8ae8620a157fac635c5e5f81ca8fb54196cdd88f
ThiagoCaiado <52451217+ThiagoCaiado@users.noreply.github.com>
2019-09-04 14:48:39 -0300

    mobileui_development

--- mobileui/src/main/java/com/thiago/mobileui/BrowseActivity.kt
+++ mobileui/src/main/java/com/thiago/mobileui/BrowseActivity.kt
@@ -2,11 +2,19 @@ package com.thiago.mobileui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_browse.*
 
 class BrowseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browse)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView(){
+        recycler_projects.layoutManager = LinearLayoutManager(this)
     }
 }
